2fde1c5e590cd15d547f7bcf096ed0b002938421
dvaidya <dvaidya@DIN19000582.corp.capgemini.com>
2016-05-06 19:35:51 +0530

    SecioundTime

--- Ad1/src/test/java/com/Test/First_Demo.java
+++ Ad1/src/test/java/com/Test/First_Demo.java
@@ -1,17 +1,25 @@
 package com.Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class First_Demo {
 
+	public static WebDriver driver;
 	@Test
-	public  void PrintHello()
+	public  void PrintHello() throws InterruptedException
 	{
 		
 	
-		System.out.println("Heloo world");
-		
-		
+		System.out.println("Heloo world 1");
+		 driver = new FirefoxDriver();
+		 driver.get("https://www.google.co.in");
+		 driver.manage().window().maximize();
+		 Thread.sleep(10000);
+		 System.out.println("Heloo world 2");
+		 driver.close();
+		 driver.quit();
 		
 	}
 	
