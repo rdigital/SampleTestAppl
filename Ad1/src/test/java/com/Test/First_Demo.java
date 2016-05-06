1b00c76c6819cf940b0ac632aa00c3b3204c1827
dvaidya <dvaidya@DIN19000582.corp.capgemini.com>
2016-05-06 19:09:42 +0530

    MavenTest_MVN

---
+++ Ad1/src/test/java/com/Test/First_Demo.java
@@ -0,0 +1,18 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+public class First_Demo {
+
+	@Test
+	public  void PrintHello()
+	{
+		
+	
+		System.out.println("Heloo world");
+		
+		
+		
+	}
+	
+}
